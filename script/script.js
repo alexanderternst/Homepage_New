9af3d794be30336448a9b2dc9bfc9b0f036c237d
Alexander Ernst <96046094+alexanderternst@users.noreply.github.com>
2024-05-14 09:27:53 +0200

    improve js

--- script/script.js
+++ script/script.js
@@ -1,18 +1,21 @@
+'use strict';
+
 // add class navbarDark on navbar scroll
 const header = document.querySelector('.navbar');
 window.onscroll = function() {
     const top = window.scrollY;
-    if(top >=100) {
+    if(top >= 100) {
         header.classList.add('navbarDark');
     }
     else {
         header.classList.remove('navbarDark');
     }
 }
+
 //collapse navbar after click on small devices
-const navLinks = document.querySelectorAll('.nav-item')
-const menuToggle = document.getElementById('navbarSupportedContent')
+const navLinks = document.querySelectorAll('.nav-item');
+const menuToggle = document.getElementById('navbarSupportedContent');
 
 navLinks.forEach((l) => {
-    l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
-})
+    l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() });
+});
